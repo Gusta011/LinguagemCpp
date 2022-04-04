41df08987572fc02ec4a4f6b88d323ca1715c1ea
Gustavo de Lucas <gustavodelucas09@gmail.com>
2022-04-04 08:53:32 -0300

    Estudo C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,17 @@
+/*
+Programa para exibir uma mensagem. O código foi
+escrito em c++
+*/
+/*
+Importação da biblioteca de entrada e saída de dados
+*/
+#include <iostream>
+
+int main(){
+
+    //Saída padrão de dados
+    //cout(console out -> Saída na tela de console(terminal)
+    std::cout << "Bem Vindo ao C++\n";
+
+    return 0;
+}
